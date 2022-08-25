2d532338cd38f02ee29cf3c89bb36387dd6d27ab
Abhinaw Ratan <75108723+AbhinawRatan@users.noreply.github.com>
2022-08-26 00:15:15 +0530

    Add files via upload

--- server/contollers/recipieController.js
+++ server/contollers/recipieController.js
@@ -1,3 +1,5 @@
+require('../models/database');
+const Category = require('../models/category');
 
 
 
@@ -6,5 +8,44 @@
  * Homepage
 */
 exports.homepage = async(req,res)=>{
+
+   
+
     res.render('index',{tittle:'Cookaway-Home'});
 }
+
+
+
+async function insertmyCategoryData(){
+    try {
+        await Category.insertMany([
+            {
+            "name": "American",
+            "image": "american-food.jpg"
+            },
+           {
+               "name": "Indian",
+               "image": "indian-food.jpg"
+           },
+           {
+               "name": "Chinese",
+               "image": "chieese.jpg"
+           },
+           {
+               "name": "Japanese",
+               "image": "japaese.jpg"
+           },
+           {
+               "name": "Italian",
+               "image": "itly.jpg"
+           }
+             
+           
+       ]
+       );
+    } catch (error) {
+        console.log('err'+ error)
+        
+    }
+}
+insertmyCategoryData();
